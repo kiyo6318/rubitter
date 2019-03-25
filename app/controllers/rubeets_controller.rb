7dc4dd84e9481a179c3a4a9e9fc09418652ca121
前川清成 <kiyonari6318@maekawakiyonarinoMacBook-puro.local>
2019-03-25 21:08:55 +0900

    newメソッドを定義し、新規つぶやき画面を作成した

--- app/controllers/rubeets_controller.rb
+++ app/controllers/rubeets_controller.rb
@@ -1,4 +1,22 @@
 class RubeetsController < ApplicationController
   def index
+  	#@rubeets = Rubeet.all
   end
+
+  def new
+  	@rubeet = Rubeet.new
+  end
+
+  def create
+  	@rubeet = Rubeet.new(rubeet_params)
+  	#if @rubeet.save
+		#	redirect_to new_rubeet_path,notice:"つぶやきました！"
+		#else
+		#	render 'new'
+		#end
+  end
+  #private
+  #def rubeet_params
+  	#params.require(:rubeet).permit(:content)
+  #end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get 'rubeets',to:'rubeets#index'
+  resources :rubeets
 end
