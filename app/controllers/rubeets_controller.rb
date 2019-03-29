2c70e6c93f00ade3da6a7da423c20ee5cbbd3331
前川清成 <kiyonari6318@maekawakiyonarinoMacBook-puro.local>
2019-03-29 14:12:44 +0900

    インデント修正2

--- app/controllers/rubeets_controller.rb
+++ app/controllers/rubeets_controller.rb
@@ -1,54 +1,54 @@
 class RubeetsController < ApplicationController
-before_action :set_rubeet,only:[:edit,:update,:destroy]
+  before_action :set_rubeet,only:[:edit,:update,:destroy]
 
-def index
-	@rubeets = Rubeet.all
-end
+  def index
+  	@rubeets = Rubeet.all
+  end
 
-def new
-	if params[:back]
-		@rubeet = Rubeet.new(rubeet_params)
-	else
-		@rubeet = Rubeet.new
-	end
-end
+  def new
+  	if params[:back]
+  		@rubeet = Rubeet.new(rubeet_params)
+  	else
+  		@rubeet = Rubeet.new
+  	end
+  end
 
-def create
-	@rubeet = Rubeet.new(rubeet_params)
-	if @rubeet.save
-		redirect_to rubeets_path,notice:"つぶやきました！"
-	else
-		render 'new'
-	end
-end
+  def create
+  	@rubeet = Rubeet.new(rubeet_params)
+  	if @rubeet.save
+  		redirect_to rubeets_path,notice:"つぶやきました！"
+  	else
+  		render 'new'
+  	end
+  end
 
-def edit
-end
+  def edit
+  end
 
-def update
-	if @rubeet.update(rubeet_params)
-		redirect_to rubeets_path,notice:"つぶやきを編集しました"
-	else
-		render 'edit'
-	end
-end
+  def update
+  	if @rubeet.update(rubeet_params)
+  		redirect_to rubeets_path,notice:"つぶやきを編集しました"
+  	else
+  		render 'edit'
+  	end
+  end
 
-def destroy
-	@rubeet.destroy
-	redirect_to rubeets_path,notice:"つぶやきを削除しました"
-end
+  def destroy
+  	@rubeet.destroy
+  	redirect_to rubeets_path,notice:"つぶやきを削除しました"
+  end
 
-def confirm
-	@rubeet = Rubeet.new(rubeet_params)
-	render :new if @rubeet.invalid?
-end
+  def confirm
+  	@rubeet = Rubeet.new(rubeet_params)
+  	render :new if @rubeet.invalid?
+  end
 
-private
-def rubeet_params
-	params.require(:rubeet).permit(:content)
-end
+  private
+  def rubeet_params
+  	params.require(:rubeet).permit(:content)
+  end
 
-def set_rubeet
-	@rubeet = Rubeet.find(params[:id])
-end
+  def set_rubeet
+  	@rubeet = Rubeet.find(params[:id])
+  end
 end
